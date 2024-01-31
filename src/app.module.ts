466b50db1164743567a8573f354e0ea1c373fc99
ymHan <onyxsard@gmail.com>
2024-01-31 15:42:52 +0900

    [ymhan]

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { SectorModule } from './api/sector/sector.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.prod',
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.prod.dev' : '.env.prod.prod',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
     }),
     TypeOrmModule.forRoot(ormConfig[0]),
